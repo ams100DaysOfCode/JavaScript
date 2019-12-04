9af81ba7a942505873150982efd42dd04fbd2301
Ahesanul Mahbub Shovon <33129132+ams-cse@users.noreply.github.com>
2019-12-04 23:03:11 +0600

    Lavel2 Updating...

--- javascript-lavel-2.js
+++ javascript-lavel-2.js
@@ -20,10 +20,12 @@ var jane = Object.create(personProto,
 });
 */
 
+
+
 // Premitives vs object
 
 // Premitives
-
+/*
 let a = 20;
 let b = a;
  a = 46;
@@ -56,3 +58,40 @@ change(age,obj);
 console.log(age);
 console.log(obj.city);
 console.log('My name is : '+ obj.name);
+*/
+
+
+// Passing function as arguments
+
+var years = [1990, 1965, 1937, 2005, 1998];
+
+function arrayCalc(arr, fn) {
+    var arrRes = [];
+    for (var i=0; i<arr.length; i++) {
+        arrRes.push(fn(arr[i]));
+    }
+    return arrRes;
+}
+
+function calculateAge(el){
+    return 2016 - el; 
+}
+
+function isFullAge(el) {
+    return el >= 18;
+}
+
+function maxHeartRate(el) {
+    if(el>=18 && el <= 81) {
+        return Math.random(206.9 - (0.67 * el));
+    }else {
+        return -1;
+    }
+}
+
+var ages = arrayCalc(years, calculateAge);
+var fullAge = arrayCalc(ages, isFullAge);
+var rates = arrayCalc(ages, maxHeartRate);
+console.log(ages);
+// console.log(fullAge);
+console.log(rates);
